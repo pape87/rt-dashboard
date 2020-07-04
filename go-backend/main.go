5480243083860052fd70c7af75411ea90db0ad1b
Luca Perotti <lperotti@ranorex.com>
2020-07-04 20:45:39 +0200

    feat(go-backend): setup docker-compose dev env

---
+++ go-backend/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"rt-dashboard/go-backend/server"
+)
+
+func main() {
+	fmt.Println("Rest API v2.0")
+	server.Server()
+
+}
---
+++ go-backend/server/server.go
@@ -0,0 +1,9 @@
+package server
+
+import (
+	"fmt"
+)
+
+func Server() {
+	fmt.Println("Run run run")
+}
